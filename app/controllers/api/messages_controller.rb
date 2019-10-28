c49a1d85f4e7306e739b9a614eda052242ae7357
KeiFunahashi <fnhskei@yahoo.co.jp>
2019-10-28 19:20:50 +0900

    自動更新が無限ループしていたので、修正しました

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,7 @@
 class Api::MessagesController < ApplicationController
   def index
-    @group = Group.find(params[:group_id]) #今いるグループの情報をパラムスの値を元にDBから取得。
-    @messages = @group.messages.includes(:user).where('id > ?', params[:last_id]) #グループが所有しているメッセージの中から、params[:last_id]よりも大きいidがないかMessageから検索して、@messagesに代入。
+    group = Group.find(params[:group_id])
+    last_message_id = params[:last_id].to_i
+    @messages = group.messages.includes(:user).where("id > #{last_message_id}")
   end
 end
